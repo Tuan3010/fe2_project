6c6c489754b9d91431ab67389801f0d3c4e9c718
Tuan3010 <tuanly.0234@gmail.com>
2024-05-20 20:50:26 +0700

    first commit

---
+++ src/app/(components)/home/page.tsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+function HomePage(){
+  return (
+    <div>
+      <h1>Tuấn</h1>
+    </div>
+  )
+}
+export default HomePage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./my_grid.css"
 import "./globals.css";
-
+import Header from "./(components)/layout/Header";
+import Footer from "./(components)/layout/Footer";
+// font chữ
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header></Header>
+        {children}
+        <Footer></Footer>
+      </body>
     </html>
   );
 }
